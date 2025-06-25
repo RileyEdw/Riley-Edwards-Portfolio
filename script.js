a2124cb7227d82843b01a0d58cc3f76ad8914109
RileyEdw <edwards.riley777@gmail.com>
2025-06-25 15:36:56 -0500

    time

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 let slideIndex = 1;
-
-// Next/previous controls
+let time = new Date();
+let hour = time.getHours()
+let greeting = ""
+console.log(hour)
+if(5<hour && hour<=11){greeting="Good Morning!"}
+else if(12<hour && hour<=16){greeting="Good Afternoon!"}
+else if(17<hour || hour<=4){greeting="Good Evening!"}
+else{greeting="Hello!"}
+// Next/previous controls 17 is evening start 16 is afternoon end 12 is afternoon begin 5 is morning begin, 11 is morning end
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
-
 
 function showSlides(n) {
   let i;
@@ -17,7 +23,7 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   if(slideIndex===1){
     document.querySelector(".slidetitle").innerHTML="Me";
-    document.querySelector(".slidedesc").innerHTML="Hey there! I'm Riley Edwards. I'm a young man from Southern Indiana, with a passion for computer science.  I like to help people when I can."
+    document.querySelector(".slidedesc").innerHTML=greeting+ " I'm Riley Edwards.<br>I'm a young man from Evansville, with a passion for computer science.  I like to help people when I can."
   };
   if(slideIndex===2){
     document.querySelector(".slidetitle").innerHTML="Code";
@@ -25,6 +31,6 @@ function showSlides(n) {
   };
   if(slideIndex===3){
     document.querySelector(".slidetitle").innerHTML="Cat";
-    document.querySelector(".slidedesc").innerHTML="Short placeholder text about something else, haven't figured it out yet."
+    document.querySelector(".slidedesc").innerHTML="Short placeholder text about something else, haven't figured what it is<br> out yet."
   };
 }
