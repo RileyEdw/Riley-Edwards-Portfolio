f5779649e6d5a93b8363f2df58b03b23dbb26361
RileyEdw <edwards.riley777@gmail.com>
2025-06-25 13:30:28 -0500

    just in case save

--- script.js
+++ script.js
@@ -17,11 +17,11 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   if(slideIndex===1){
     document.querySelector(".slidetitle").innerHTML="Me";
-    document.querySelector(".slidedesc").innerHTML="Short placeholder text about me."
+    document.querySelector(".slidedesc").innerHTML="Hey there! I'm Riley Edwards. I'm a young man from Southern Indiana, with a passion for computer science.  I like to help people when I can."
   };
   if(slideIndex===2){
     document.querySelector(".slidetitle").innerHTML="Code";
-    document.querySelector(".slidedesc").innerHTML="Short placeholder text about coding."
+    document.querySelector(".slidedesc").innerHTML="I've been coding since I was a kid, although it was simple block code. I found a love for it back then. Since, I've been growing my knowledge and skill through practice and projects."
   };
   if(slideIndex===3){
     document.querySelector(".slidetitle").innerHTML="Cat";
